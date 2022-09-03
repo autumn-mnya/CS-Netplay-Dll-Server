82d3772a7e1f32d4d0c87e0dfb7df0fdb50c7fa2
Kaitlyn <49522306+ItsKaitlyn@users.noreply.github.com>
2022-09-03 19:15:22 -0400

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -175,6 +175,7 @@ void HandleServerEvent(ENetEvent event)
 						//if(HIDE_AND_SEEK)
 						//   do something to overwrite the client name with 0's?
 						repPacketData->Write(clients[i].name, 1, MAX_NAME);	//name
+						repPacketData->WriteLE32(packetData->ReadLE32());       //client's player num
 						repPacketData->WriteLE32(packetData->ReadLE32());		//x
 						repPacketData->WriteLE32(packetData->ReadLE32());		//y
 						repPacketData->WriteLE32(packetData->ReadLE32());		//up
@@ -189,7 +190,6 @@ void HandleServerEvent(ENetEvent event)
 						repPacketData->WriteLE32(packetData->ReadLE32());		//stage
 						repPacketData->WriteLE32(packetData->ReadLE32());		//mim
 						repPacketData->WriteLE32(packetData->ReadLE32());		//hide_vp_on_map / hide_me_on_map
-						repPacketData->WriteLE32(packetData->ReadLE32());       //client's player num
 						delete repPacketData;
 
 						for (int v = 0; v < MAX_CLIENTS; v++)
